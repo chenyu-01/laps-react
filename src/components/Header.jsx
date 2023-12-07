b8b3829bf59a4c0146caf06cc58d42cdc5ca08a8
chenyu-01 <chenyu101@outlook.com>
2023-12-07 23:14:44 +0800

    change line ends and create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { AppBar, Toolbar, Drawer, List, ListItem } from '@mui/material'
+import { React } from 'react'
+
+export default function Header() {
+  return (
+    <AppBar>
+      <Toolbar>{/* Add your header content here */}</Toolbar>
+    </AppBar>
+  )
+}
