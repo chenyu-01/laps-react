238c999cbd52e9abc0ae1980d2ed1a8a0eb3b36d
chenyu-01 <chenyu101@outlook.com>
2023-12-12 01:53:51 +0800

    Ajust small screen design

--- src/components/Authentification/Layout.jsx
+++ src/components/Authentification/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 export default function Layout({ children }) {
   return (
-    <div className="flex justify-center flex-col max-w-[1396px] bg-white items-center px-16 py-12 rounded-[40px]">
+    <div className="flex justify-center flex-col max-w-[1396px] bg-white items-center sm:px-16 px-3 py-12 rounded-[40px]">
       <Header></Header>
       <div className="flex w-full flex-col mb-40 items-start max-md:max-w-full max-md:my-10">
         <div className="self-stretch flex items-stretch justify-between gap-5 mt-20 max-md:max-w-full max-md:flex-wrap">
