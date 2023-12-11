62e238e67559994b4b284c90d91463aa8cb38a40
chenyu-01 <chenyu101@outlook.com>
2023-12-11 10:59:54 +0800

    changed browser router

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Navigate } from 'react-router-dom';
 function App() {
   return (
     <div className="flex justify-center">
-      <BrowserRouter>
+      <BrowserRouter basename="/chenyu-01/laps-react">
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
