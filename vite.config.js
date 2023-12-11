c67b0d35626d4a371f70136107c62d23abf7962e
chenyu-01 <chenyu101@outlook.com>
2023-12-11 10:47:49 +0800

    Add base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chenyu-01/laps-react',
+
   plugins: [react()],
   test: {
     // Your test configuration
