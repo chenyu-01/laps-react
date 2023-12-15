ca17358207a52f86a44e90abad5b7c57a5eb1e3e
preethivenkat5 <preethivenkat5@gmail.com>
2023-12-16 00:18:37 +0800

    reverted flex removal change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppRoutes from './AppRouts';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
-    <div className>
+    <div className="flex">
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
