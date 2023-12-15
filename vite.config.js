fa74c7bd1d7e451cf2da76de630721ec7be2a71f
chenyu-01 <chenyu101@outlook.com>
2023-12-15 16:15:17 +0800

    change base url to let backend serve

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/laps-react/',
+  // base: '/laps-react/',
 
   plugins: [react()],
   test: {
