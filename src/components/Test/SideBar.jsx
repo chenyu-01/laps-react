cab96e67326db7f89e1e9c3d8c385f388144b9fc
chenyu-01 <chenyu101@outlook.com>
2023-12-17 16:33:12 +0800

    change SideBar Links

--- src/components/Test/SideBar.jsx
+++ src/components/Test/SideBar.jsx
@@ -1,17 +1,64 @@
-import { React } from 'react';
+import { React, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 
-export default function sideBar() {
+export default function SideBar() {
+  const { userData } = useContext(AuthContext);
+  const role = userData.role;
   return (
-    <div className="flex flex-col items-stretch w-[21%] max-md:w-full max-md:ml-0">
-      <div className="shadow-2xl bg-white flex w-full grow flex-col justify-center mx-auto pl-9 pr-16 py-12 items-start max-md:px-5">
-        <div className="flex items-stretch gap-3 mt-14 mb-[472px] max-md:my-10">
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/3a8ad1e84b7786370863bd4fd8dbb35930e092231a4f0fddb4e57cca5ea355de?apiKey=61d501c9e4204f7ab9004ebff2b8b115&"
-            className="aspect-[1.11] object-contain object-center w-[21px] overflow-hidden shrink-0 max-w-full"
-          />
-        </div>
+    <div className="flex flex-col bg-white items-stretch w-[21%] max-md:w-full max-md:ml-0">
+      <div className="shadow-2xl flex w-full grow flex-col mx-auto px-8 py-4 items-start max-md:px-5 divide-y-5">
+        {role === 'Employee' ? <NavigationEmployee /> : null}
+        {role === 'Admin' ? <NavigationAdmin /> : null}
+        {role === 'Manager' ? <NavigationManager /> : null}
       </div>
+    </div>
+  );
+}
+
+function NavLink({ children }) {
+  return (
+    <div className="text-zinc-700 text-opacity-80 text-base font-medium whitespace-nowrap justify-center items-stretch bg-white rounded-3xl max-md:px-5 py-3">
+      {children}
+    </div>
+  );
+}
+
+function NavigationEmployee() {
+  return (
+    <div>
+      <NavLink>
+        <a href={'http://localhost:8080/leave'}>New Leave Application</a>
+      </NavLink>
+      <NavLink>
+        <Link to={'/Admin'}>Admin Page</Link>
+      </NavLink>
+      <NavLink>
+        <Link to={'/Approval'}>Approval Page</Link>
+      </NavLink>
+    </div>
+  );
+}
+
+function NavigationAdmin() {
+  return (
+    <div>
+      <NavLink>
+        <Link to={'/Admin'}>Admin Page</Link>
+      </NavLink>
+      <NavLink>
+        <Link to={'/Approval'}>Approval Page</Link>
+      </NavLink>
+    </div>
+  );
+}
+
+function NavigationManager() {
+  return (
+    <div>
+      <NavLink>
+        <Link to={'/Approval'}>Approval Page</Link>
+      </NavLink>
     </div>
   );
 }
