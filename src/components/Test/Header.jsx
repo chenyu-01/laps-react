850622b32192dbfb6b719a4c4b70e09bfe39f204
chenyu-01 <chenyu101@outlook.com>
2023-12-17 18:10:31 +0800

    add toggle button

--- src/components/Test/Header.jsx
+++ src/components/Test/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import UserCard from './UserCard.jsx';
 
-export default function Header() {
+export default function Header({ toggleSidebar }) {
   return (
     <div className="justify-between items-center border-b-[color:var(--Dividers-and-Borders---grey-Light,#CECECE)] bg-white flex pl-16 pr-11 py-4 border-b-[0.889px] border-solid items-end max-md:max-w-full max-md:px-5">
-      <div className={'font-bold text-2xl'}>
-        <h1>Leave Application Processing System</h1>
+      <div className={''}>
+        <h1 className={'font-bold text-2xl'}>
+          Leave Application Processing System
+        </h1>
+        <div className="form-control w-52">
+          <label className="cursor-pointer label">
+            <span className="label-text">Toggle SideBar</span>
+            <input
+              type="checkbox"
+              className="toggle toggle-primary"
+              defaultChecked={'checked'}
+              onChange={toggleSidebar}
+            />
+          </label>
+        </div>
       </div>
       <div className="items-center flex gap-4 py-0.5">
         <UserCard />
