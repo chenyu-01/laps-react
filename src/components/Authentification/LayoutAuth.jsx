a3f04d97a3340554ec475593f76bd63ffb18ba0e
chenyu-01 <chenyu101@outlook.com>
2023-12-18 15:13:43 +0800

    login page justify center

--- src/components/Authentification/LayoutAuth.jsx
+++ src/components/Authentification/LayoutAuth.jsx
@@ -3,17 +3,19 @@ import Header from './Header';
 import universityImage from './university.png';
 export default function LayoutAuth({ children }) {
   return (
-    <div className="flex justify-center flex-col max-w-[1396px] bg-white items-center sm:px-16 px-3 py-12 rounded-[40px]">
-      <Header></Header>
-      <div className="flex w-full flex-col mb-40 items-start max-md:max-w-full max-md:my-10">
-        <div className="self-stretch flex items-stretch justify-between gap-5 mt-20 max-md:max-w-full max-md:flex-wrap">
-          <img
-            loading="lazy"
-            srcSet={universityImage}
-            className="object-contain object-center md:w-1/2 overflow-hidden hidden md:flex"
-          />
-          <div className="justify-center flex flex-col items-center my-auto">
-            <div className="mr-4">{children}</div>
+    <div className={'flex justify-center'}>
+      <div className="flex justify-center flex-col max-w-screen-2xl bg-white items-center sm:px-16 px-3 py-12">
+        <Header></Header>
+        <div className="flex w-full flex-col mb-40 items-start max-md:max-w-full max-md:my-10">
+          <div className="self-stretch flex items-stretch justify-between gap-5 mt-20 max-md:max-w-full max-md:flex-wrap">
+            <img
+              loading="lazy"
+              srcSet={universityImage}
+              className="object-contain object-center md:w-1/2 overflow-hidden hidden md:flex"
+            />
+            <div className="justify-center flex flex-col items-center my-auto">
+              <div className="mr-4">{children}</div>
+            </div>
           </div>
         </div>
       </div>
