cc1074596e4f69c31780b88350c6a1d13ce2184d
chenyu-01 <chenyu101@outlook.com>
2023-12-20 01:30:07 +0800

    add fetch method

--- src/components/LeaveHistory/LeaveHistory.jsx
+++ src/components/LeaveHistory/LeaveHistory.jsx
@@ -10,12 +10,66 @@ import TestLayout from '../Test/TestLayout.jsx';
 import mockLeaveApplicationList from './mocklist.js';
 
 export default function LeaveHistory() {
+  const [status, setStatus] = React.useState('All');
+  let leaveApplicationList;
+  async function fetchLeaveApplicationList() {
+    try {
+      let response;
+      switch (status) {
+        case 'All':
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application'
+          );
+          break;
+        case 'Applied':
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application?status=Applied'
+          );
+          break;
+        case 'Approved':
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application?status=Approved'
+          );
+          break;
+        case 'Rejected':
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application?status=Rejected'
+          );
+          break;
+        case 'Cancelled':
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application?status=Cancelled'
+          );
+          break;
+        case 'Updated':
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application?status=Updated'
+          );
+          break;
+        default:
+          response = await fetch(
+            'http://localhost:8080/api/applications/leave-application'
+          );
+          break;
+      }
+      if (response.ok) {
+        leaveApplicationList = await response.json();
+        console.log(leaveApplicationList);
+      } else {
+        const error = await response.json();
+        console.error(error);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  fetchLeaveApplicationList();
   return (
     <div>
       <TestLayout>
         <div className="flex flex-col mt-4 px-16 items-start max-md:max-w-full max-md:px-5 min-w-[950px]">
           <LeaveTitle />
-          <LeaveButtonCategories />
+          <LeaveButtonCategories status={status} setStatus={setStatus} />
           <div className="bg-white flex flex-col mt-3.5 pb-9 rounded-xl border-[0.889px] border-solid border-gray-200 md:w-full">
             <HeaderRow />
             {mockLeaveApplicationList.map((la) => (
