a4fec0f7703f59dadb2b67c9b26a8bf2d41988e9
chenyu-01 <chenyu101@outlook.com>
2023-12-20 17:21:42 +0800

    new Dashboard.jsx

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react';
+import Layout from './Layout.jsx';
+import { AuthContext } from '../../context/AuthContext.jsx';
+import { useContext } from 'react';
+export default function Dashboard() {
+  const { userData } = useContext(AuthContext);
+  return (
+    <Layout>
+      <div className={'prose flex flex-col mx-auto container'}>
+        <h1 className={'mt-5 mx-auto'}>Profile</h1>
+        <h2>Role: {userData.role}</h2>
+        <h2>User: {userData.name}</h2>
+        <h2>Email: {userData.email}</h2>
+      </div>
+    </Layout>
+  );
+}
