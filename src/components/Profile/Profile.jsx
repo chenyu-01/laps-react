d9747d6199c7ca9b433b35336c8a587c0c3f4daf
chenyu-01 <chenyu101@outlook.com>
2023-12-20 17:21:33 +0800

    delete picture, add link to Dashboard.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,30 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext.jsx';
-import BackgroundPicTest from '../../assets/background-pic-test.png';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 export default function Profile() {
+  const navigate = useNavigate();
   const { isAuthenticated, logout, userData } = useContext(AuthContext);
   return (
-    <div className={'prose flex flex-col items-center mx-auto container'}>
-      <h1 className={'mt-5'}>Profile Page</h1>
-      <h2>{isAuthenticated ? 'Authenticated' : 'Not Authenticated'}</h2>
-      <div>Role: {userData.role}</div>
-      <div>User: {userData.name}</div>
-      <div>Email: {userData.email}</div>
-      <Link to={'/login'} className="btn btn-primary w-full my-2">
-        Login Page
-      </Link>
+    <div className={'prose flex flex-col mx-auto container'}>
+      <h1 className={'mt-5 mx-auto'}>Profile Page</h1>
+      <h2>Role: {userData.role}</h2>
+      <h2>User: {userData.name}</h2>
+      <h2>Email: {userData.email}</h2>
       <button
         className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg w-full my-2"
         onClick={logout}
       >
         Logout
       </button>
-      <img src={BackgroundPicTest} alt="BackgroundPicTest" />
+      <button
+        className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg w-full my-2"
+        onClick={(event) => {
+          event.preventDefault();
+          navigate('/dashboard');
+        }}
+      >
+        Dashboard
+      </button>
     </div>
   );
 }
