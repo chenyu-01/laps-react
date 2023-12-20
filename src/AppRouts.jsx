4b66baf219e2d763c9075ec796d93b876f558667
chenyu-01 <chenyu101@outlook.com>
2023-12-20 17:22:23 +0800

    new routes

--- src/AppRouts.jsx
+++ src/AppRouts.jsx
@@ -8,8 +8,8 @@ import Approval from './components/Approval/Approval';
 import Admin from './components/Admin/Admin';
 import Profile from './components/Profile/Profile.jsx';
 import LeaveHistory from './components/LeaveHistory/LeaveHistory';
-import Layout from './components/Dashboard/Layout.jsx';
 import LeaveModify from './components/LeaveHistory/LeaveModify.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 const AppRoutes = () => {
   return (
@@ -20,8 +20,8 @@ const AppRoutes = () => {
         <Route path="/Admin" element={<Admin />} />
         <Route path="/Approval" element={<Approval />} />
         <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/dashboard" element={<Layout />} />
-        <Route path="/test-dashboard" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/leavehistory" element={<LeaveHistory />} />
         <Route path="/leave-modify/:leaveId" element={<LeaveModify />} />
         {/* ... other routes ... */}
