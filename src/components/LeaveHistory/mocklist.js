2c2754a2fad3a84b6dcb020a6e62f172e0a81d9a
chenyu-01 <chenyu101@outlook.com>
2023-12-22 05:08:32 +0800

    testpush

--- src/components/LeaveHistory/mocklist.js
+++ src/components/LeaveHistory/mocklist.js
@@ -1,6 +1,7 @@
 let mockLeaveApplicationList = [];
 for (let i = 0; i < 10; i++) {
   const mockLeaveApplication = {
+    testchange: 'testchange',
     leaveId: i,
     type: 'Annual',
     startDate: '2010-09-04',
